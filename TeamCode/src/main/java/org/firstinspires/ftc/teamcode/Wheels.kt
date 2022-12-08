52071ae61be27729d6ce18dd2d44008dbaed5173
cadence <cadence.forney21@gmail.com>
2022-12-07 19:22:49 -0500

    hope it works
    trying to define motors and such

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Wheels.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Wheels.kt
@@ -4,6 +4,8 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import com.qualcomm.robotcore.hardware.DcMotor
 import org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap
 
+@TeleOp(name = "Move")
+
 class Wheels {
 
     private var motor1: DcMotor? = null
@@ -11,15 +13,28 @@ class Wheels {
     private var motor3: DcMotor? = null
     private var motor4: DcMotor? = null
 
-    fun runOpMode() {
+    fun init() {
 
         motor1 = hardwareMap.get<DcMotor>(DcMotor::class.java, "motor1")
         motor2 = hardwareMap.get<DcMotor>(DcMotor::class.java, "motor2")
         motor3 = hardwareMap.get<DcMotor>(DcMotor::class.java, "motor3")
         motor4 = hardwareMap.get<DcMotor>(DcMotor::class.java, "motor4")
 
-        
-
     }
 
+    fun power() {
+
+        this.motor1!!.power = 0.5
+        this.motor2!!.power = 0.5
+        this.motor3!!.power = 0.5
+        this.motor4!!.power = 0.5
+
+    }
+    fun stop(){
+
+        this.motor1!!.power = 0.0
+        this.motor2!!.power = 0.0
+        this.motor3!!.power = 0.0
+        this.motor4!!.power = 0.0
+    }
 }

49fa1a1a60c04df6390c91f55b88eaf702320dcc
cadence <cadence.forney21@gmail.com>
2022-12-18 02:32:04 -0500

    Simple teleOp reworked
    I changed it so you do not need the null part and fixed the weird looking power code :)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Wheels.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Wheels.kt
@@ -5,19 +5,15 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import com.qualcomm.robotcore.hardware.DcMotor
 
 @TeleOp(name = "Move")
+//@Disabled
 class Wheels: LinearOpMode() {
-    //sets the motors to have no value so they do not explode
-    var motor1: DcMotor? = null
-    var motor2: DcMotor? = null
-    var motor3: DcMotor? = null
-    var motor4: DcMotor? = null
 
     override fun runOpMode() {
         //initualizes the motors
-        motor1 = hardwareMap.get(DcMotor::class.java, "motor1")
-        motor2 = hardwareMap.get(DcMotor::class.java, "motor2")
-        motor3 = hardwareMap.get(DcMotor::class.java, "motor3")
-        motor4 = hardwareMap.get(DcMotor::class.java, "motor4")
+        val motor1 = hardwareMap.get(DcMotor::class.java, "motor1")
+        val motor2 = hardwareMap.get(DcMotor::class.java, "motor2")
+        val motor3 = hardwareMap.get(DcMotor::class.java, "motor3")
+        val motor4 = hardwareMap.get(DcMotor::class.java, "motor4")
 
         //this writes on the phone
         //may need more here but i am not sure how to impliment
@@ -28,17 +24,14 @@ class Wheels: LinearOpMode() {
         waitForStart()
 
         while (opModeIsActive()) {
-            //this says hey if we move the joystick set it to that power
-            //the toDouble sets it as a number so it works
-            var right = (-gamepad1.right_stick_x.toDouble())
-            var left = (gamepad1.right_stick_y.toDouble())
+            //might need to just set the motor powers to a number i am not sure
+            var pow = gamepad1.right_stick_x
 
-            this.motor1!!.power = left
-            this.motor2!!.power = right
-            this.motor3!!.power = left
-            this.motor4!!.power = right
+            motor1.power = pow.toDouble()
+            motor2.power = pow.toDouble()
+            motor3.power = pow.toDouble()
+            motor4.power = pow.toDouble()
         }
     }
 }
-//this could all be wrong i am really not sure 
 

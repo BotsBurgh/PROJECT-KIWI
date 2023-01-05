222f8e0759793f80312b98e335946737e4d22970
cadence <cadence.forney21@gmail.com>
2023-01-04 21:10:01 -0500

    pew forward pew
    moves the robot forward

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Wheels.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Wheels.kt
@@ -15,6 +15,8 @@ class Wheels: LinearOpMode() {
         val motor3 = hardwareMap.get(DcMotor::class.java, "motor3")
         val motor4 = hardwareMap.get(DcMotor::class.java, "motor4")
 
+        //motor1.setDirection(DcMotor.Direction.REVERSE)
+
         //this writes on the phone
         //may need more here but i am not sure how to impliment
         telemetry.addData("Status", "Initialized")
@@ -25,12 +27,13 @@ class Wheels: LinearOpMode() {
 
         while (opModeIsActive()) {
             //might need to just set the motor powers to a number i am not sure
-            var pow = gamepad1.right_stick_x
+            var pow = gamepad1.right_stick_x.toDouble()
+            var pew = gamepad1.left_stick_y.toDouble()
 
-            motor1.power = pow.toDouble()
-            motor2.power = pow.toDouble()
-            motor3.power = pow.toDouble()
-            motor4.power = pow.toDouble()
+            motor1.power = -pew
+            motor2.power = pew
+            motor3.power = pew
+            motor4.power = -pew
         }
     }
 }
